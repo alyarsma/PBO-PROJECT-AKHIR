c3790e5804460ffdee437dead1e2e2c582634561
nailaaf <nailaazwaf888@gmail.com>
2025-11-25 13:02:24 +0700

    model product order keranjang

---
+++ PBO PROJECT AKHIR/Models/Order.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PBO_PROJECT_AKHIR.Models
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public string Alamat { get; set; }
+        public int JumlahItem { get; set; }
+        public int SubTotal { get; set; }
+        public StatusPesanan Status { get; set; }
+    }
+
+    public enum StatusPesanan
+    {
+        [Description("Menunggu Dikonfirmasi")]
+        MenungguDikonfirmasi,
+
+        [Description("Dikirim")]
+        Dikirim,
+
+        [Description("Selesai")]
+        Selesai,
+
+        [Description("Dibatalkan")]
+        Dibatalkan
+    }
+}
---
+++ PBO PROJECT AKHIR/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PBO_PROJECT_AKHIR.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public byte[] Image { get; set; }
+        public int Price { get; set; }
+        public int Stock { get; set; }
+        public int KeranjangId { get; set; }
+    }
+}
