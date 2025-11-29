0a18536bad69fd0e13c83c3ff36caab1cf409f87
alyarsma <alyanadillaa@gmail.com>
2025-11-29 16:26:22 +0700

    profiluser

---
+++ PBO PROJECT AKHIR/Views/User/ProfilUser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PBO_PROJECT_AKHIR.Views.User
+{
+    public partial class ProfilUser : Form
+    {
+        public ProfilUser()
+        {
+            InitializeComponent();
+        }
+
+        private void ProfilUser_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
